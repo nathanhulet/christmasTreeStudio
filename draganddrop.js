1ba3c70071fde8cf4e9518241ea903238ed944fb
Nathan M Hulet <nmh91@uakron.edu>
2021-11-25 20:17:21 -0500

    Add responsive navbar

--- draganddrop.js
+++ draganddrop.js
@@ -1,39 +1,8 @@
-var count = 0;
-
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("childID", ev.target.id);
-}
-
-function drop(ev) {
-  ev.preventDefault();
-
-  const id = ev.dataTransfer.getData("childID");
-
-  if (id.startsWith("dragged") || !id) {
-    return;
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-
-  const nodeCopy = document.getElementById(id).cloneNode(true);
-
-  nodeCopy.id = "dragged" + id + count++;
-
-  nodeCopy.addEventListener("dragstart", drag);
-
-  ev.target.appendChild(nodeCopy);
-}
-
-function deleteDiv(ev) {
-  ev.preventDefault();
-
-  const id = ev.dataTransfer.getData("childID");
-
-  if (!id.startsWith("dragged")) {
-    return;
-  }
-  const el = document.getElementById(id);
-  el.parentNode.removeChild(el);
 }
