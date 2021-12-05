b3682a9a634e8382965b0118ca20ecbc3e779864
T-pin <89922510+T-pin@users.noreply.github.com>
2021-12-05 12:52:44 -0500

    Add a third Andrew tree into .sql

--- studio-db.sql
+++ studio-db.sql
@@ -18,4 +18,5 @@ CREATE TABLE Design (
 
 insert into Design values
 (1, 'My First Tree', 2, 2, 3, 2, 0, 1, false),
-(2, 'I love this studio', 3, 2, 1, 1, 0, 2, true);
+(2, 'I love this studio', 3, 2, 1, 1, 0, 2, true),
+(3, 'Andrew', 1, 2, 1, 0, 1, 1, true);
